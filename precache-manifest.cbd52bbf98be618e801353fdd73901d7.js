3b78fd2788cf518cb44494950b72721e05592296
Raghu Krishnamurthy <kichyrag76@gmail.com>
2020-04-19 13:56:32 -0400

    Updates

---
+++ precache-manifest.cbd52bbf98be618e801353fdd73901d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e75616c43c160c041a705b0adcd9f4da",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "7a88014525018a945fe7",
+    "url": "/monster-rolodex/static/css/main.e67ea769.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/monster-rolodex/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a88014525018a945fe7",
+    "url": "/monster-rolodex/static/js/main.0178b5bd.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
